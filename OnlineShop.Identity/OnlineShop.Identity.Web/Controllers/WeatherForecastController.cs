69507eccc881581c66433f8c8871d4d63a3bf363
Kola Serko <kol4serko@gmail.com>
2021-12-04 18:09:39 -0800

    fix

--- OnlineShop.Identity/OnlineShop.Identity.Web/Controllers/WeatherForecastController.cs
+++ OnlineShop.Identity/OnlineShop.Identity.Web/Controllers/WeatherForecastController.cs
@@ -3,11 +3,12 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace OnlineShop.Identity.Web.Controllers
 {
+    [Route("api/[controller]")]
     [ApiController]
-    [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
@@ -22,17 +23,18 @@ namespace OnlineShop.Identity.Web.Controllers
             _logger = logger;
         }
 
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet("/api/WeatherForecast/Get")]
+        public async Task<IActionResult> Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            return Ok("jggjgjh");
+        }
+
+        [HttpPost("/api/WeatherForecast/post")]
+        public async Task<IActionResult> JDSHF()
+        {
+            var rng = new Random();
+            return Ok("jggjgjh");
         }
     }
 }
